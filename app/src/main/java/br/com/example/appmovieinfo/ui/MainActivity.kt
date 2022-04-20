26b7875454484d689b9ca6cf778c9c077359b87f
JKalebe <jeremiaskalebe@gmail.com>
2022-04-20 14:54:11 -0300

    commit

--- app/src/main/java/br/com/example/appmovieinfo/ui/MainActivity.kt
+++ app/src/main/java/br/com/example/appmovieinfo/ui/MainActivity.kt
@@ -1,26 +1,14 @@
 package br.com.example.appmovieinfo.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import br.com.example.appmovieinfo.R
-import br.com.example.appmovieinfo.model.Movie
-import br.com.example.appmovieinfo.model.MovieHttp
-import br.com.example.appmovieinfo.ui.adapter.MovieListAdapter
 import br.com.example.appmovieinfo.ui.adapter.MoviePagerAdapter
-import br.com.example.appmovieinfo.ui.viewmodel.MovieListViewModel
 import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
